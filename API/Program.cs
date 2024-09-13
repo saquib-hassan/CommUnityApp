fe7d25b34756dbe4893e614896a22d9ef281f5db
Saquib Hassan <hsakib1244@outlook.com>
2024-09-13 16:15:24 +0600

    deleted

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,3 @@
-using API.Data;
-using API.Entities;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +7,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(opt=>
-{
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 var app = builder.Build();
 
