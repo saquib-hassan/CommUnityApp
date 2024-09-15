d0dc7923c3caffb06ca13e718b2b5aff1eff1445
Saquib Hassan <hsakib1244@outlook.com>
2024-09-15 10:57:09 +0600

    login controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,10 +38,23 @@ namespace API.Controllers
 
         }
 
-        [HttpPost]
-        public async Task<ActionResult<AppUser>> Login()
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
         {
-            
+            var user = await context.Users.FirstOrDefaultAsync(x=> x.UserName == loginDto.Username.ToLower());
+
+            if(user == null) return Unauthorized("Invalid Username");
+
+            using var hmack = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmack.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+            return user;
+
         }
         private async Task<bool> UserExists(string username)
         {
