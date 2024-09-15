44f15003b54c036ae5a2395c4e21019f72fc8a4a
Saquib Hassan <hsakib1244@outlook.com>
2024-09-15 12:02:54 +0600

    Service injected

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 
 builder.Services.AddCors();
+
+builder.Services.AddScoped<ITokenService,TokenService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
