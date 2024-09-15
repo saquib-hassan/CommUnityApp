e6ccd80437144c34d0c052ab4311411d17283e18
Saquib Hassan <hsakib1244@outlook.com>
2024-09-15 21:01:35 +0600

    controller modified with UserDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,17 +8,18 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context) : BaseApiController
+    public class AccountController(DataContext context,ITokenService tokenService) : BaseApiController
     {
 
         [HttpPost("register")]       
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if(await UserExists(registerDto.Username))return BadRequest("Username already exists");
 
@@ -34,12 +35,15 @@ namespace API.Controllers
 
             context.Users.Add(user);
             await context.SaveChangesAsync();
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                TokenKey = tokenService.CreateToken(user)
+            };
 
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await context.Users.FirstOrDefaultAsync(x=> x.UserName == loginDto.Username.ToLower());
 
@@ -53,7 +57,10 @@ namespace API.Controllers
             {
                 if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
             }
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                TokenKey = tokenService.CreateToken(user)
+            };
 
         }
         private async Task<bool> UserExists(string username)
