14b9025f59cc06192883c3d3766c4b5a60f7e9f6
Saquib Hassan <hsakib1244@outlook.com>
2024-09-15 20:42:52 +0600

    tokenServiceImplemented

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
@@ -15,9 +20,25 @@ namespace API.Services
             var tokenKey = config["TokenKey"] ?? throw new Exception("Can't access tokenKey from appsetting");
             if(tokenKey.Length < 64)  throw new Exception("Token key must be longer than 64");
 
-            var key = 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.NameIdentifier, user.UserName)
+            };
 
 
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
