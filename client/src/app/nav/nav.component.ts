ec3d01fff3f5bafa9103f018859a89605c8b9fc9
Saquib Hassan <hsakib1244@outlook.com>
2024-09-17 19:03:51 +0600

    BsDropdownModule

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_service/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
