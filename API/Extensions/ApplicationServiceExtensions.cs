43ddc085498a0ff3d3e6b2befb6c0d611122ce48
Saquib Hassan <hsakib1244@outlook.com>
2024-09-19 12:45:10 +0600

    reverted

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,6 +14,7 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationService(this IServiceCollection services,
          IConfiguration config)
         {
+            services.AddControllers();
 
             // services.AddControllers().AddJsonOptions(options =>
             // {
